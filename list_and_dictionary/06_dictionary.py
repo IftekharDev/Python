a21b31e9468f9b822077f51fb7badb3764a8fb00
Iftekhar <iftars2774@gmail.com>
2025-07-12 14:24:57 +0530

    uncommented the commented codes

--- list_and_dictionary/06_dictionary.py
+++ list_and_dictionary/06_dictionary.py
@@ -4,19 +4,19 @@
 items() returns a list of tuples where each tuple is a tuple of key-value pair.
 '''
 
-# my_dict = {
-#     'name':'Ben',
-#     'age':'11',
-#     'city':'New york'}
+my_dict = {
+    'name':'Ben',
+    'age':'11',
+    'city':'New york'}
 
-# print(my_dict.items())
+print(my_dict.items())
 
 '''
 Getting key and value separately using .items()
 '''
 
-# for k,v in my_dict.items():
-#    print(k,v)
+for k,v in my_dict.items():
+   print(k,v)
 
 
 #### ----------- use of .clear() ---------------------------
@@ -26,13 +26,13 @@ In Python, the .clear() method is used to remove all elements from a mutable seq
 such as a list, set, or dictionary.
 '''
 
-# my_dict = {
-#     'name':'Ben',
-#     'age':'11',
-#     'city':'New york'}
+my_dict = {
+    'name':'Ben',
+    'age':'11',
+    'city':'New york'}
 
-# my_dict.clear()
-# print(my_dict)
+my_dict.clear()
+print(my_dict)
 
 
 #### ----------- use of .pop()------------------------------
@@ -41,47 +41,47 @@ such as a list, set, or dictionary.
 The dict.pop() method is used to remove a key and it's value from a dictionary.
 '''
 
-# my_dict = {
-#     'name':'Ben',
-#     'age':'11',
-#     'city':'New york'}
-# my_dict.pop('age')
-# print(my_dict)
+my_dict = {
+    'name':'Ben',
+    'age':'11',
+    'city':'New york'}
+my_dict.pop('age')
+print(my_dict)
 
 
 
 #### ------------- empty dictionary -------------------------
 
-# emp_dict = {}   # creating an empty dictionary
-# print(emp_dict)
-# print(len(emp_dict))
+emp_dict = {}   # creating an empty dictionary
+print(emp_dict)
+print(len(emp_dict))
 
 
 #### --------------Deleting a dictionary ------------------------
 
-# my_dict = {
-#     'name':'Ben',
-#     'age':'11',
-#     'city':'New york'}
+my_dict = {
+    'name':'Ben',
+    'age':'11',
+    'city':'New york'}
 
-# del(my_dict)
-# print(my_dict)
+del(my_dict)
+print(my_dict)
 
 
 #### ------------- .copy() -------------------------
 
-# fruits = {
-#     1:'apple',
-#     2:'banana',
-#     3:'pineapple',
-#     4:'mango',
-#     5:'papaya'}
+fruits = {
+    1:'apple',
+    2:'banana',
+    3:'pineapple',
+    4:'mango',
+    5:'papaya'}
 
-# print(fruits)
+print(fruits)
 
-# dict2 = fruits.copy()
-# print(dict2)
-# print(dict == dict2)
+dict2 = fruits.copy()
+print(dict2)
+print(dict == dict2)
 
 
 
@@ -93,17 +93,17 @@ A nested dictionary in Python is simply a dictionary that contains another dicti
 This can be useful for storing more complex data structures.
 '''
 
-# pets = {
-#     "pet1": {"name": "Buddy", "type": "dog", "age": 5},
-#     "pet2": {"name": "Mittens", "type": "cat", "age": 3},
-#     "pet3": {"name": "Tweety", "type": "bird", "age": 2}
-# }
+pets = {
+    "pet1": {"name": "Buddy", "type": "dog", "age": 5},
+    "pet2": {"name": "Mittens", "type": "cat", "age": 3},
+    "pet3": {"name": "Tweety", "type": "bird", "age": 2}
+}
 
-# print(pets)
-# print(pets["pet1"])
-# print(pets["pet1"]["age"])
+print(pets)
+print(pets["pet1"])
+print(pets["pet1"]["age"])
 
-# pets["pet2"]["age"] = 4
-# print(pets)
+pets["pet2"]["age"] = 4
+print(pets)
 
 
