83094a39a3f415eb25c83b3db3f8625a8344eb75
Iftekhar <iftars2774@gmail.com>
2025-07-12 15:47:43 +0530

    few refinements

--- tuples_and_sets/01_tuples.py
+++ tuples_and_sets/01_tuples.py
@@ -9,70 +9,70 @@ Tuples can contain elements of different data types, such as integers, floats, s
 One can access individual elements of a tuple using indexing, similar to the list.
 '''
  
-# my_tuple = (1, 2, 3, 'a', 'b', 'c')
+my_tuple = (1, 2, 3, 'a', 'b', 'c')
 
-# print(my_tuple)
-# print(my_tuple[0])
-# print(my_tuple[3])
-# print(my_tuple[5])
-# print(my_tuple[1:2])
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[3])
+print(my_tuple[5])
+print(my_tuple[1:2])
 
-# print(my_tuple.index('a'))
-# print(my_tuple.index(1))
-# print(my_tuple.index('c'))
+print(my_tuple.index('a'))
+print(my_tuple.index(1))
+print(my_tuple.index('c'))
 
 
 #### ---------------------------------------------
 
-# new_tup = (1)
-# print(new_tup)
-# print(type(new_tup))
+new_tup = (1)
+print(new_tup)
+print(type(new_tup))
 
-# new_tup_1 = (1,)  ## To make a single element tuple it's necessary to add , after the element.
-# print(type(new_tup_1))
+new_tup_1 = (1,)  ## To make a single element tuple it's necessary to add , after the element.
+print(type(new_tup_1))
 
 
 #### -------count() in tuple--------------------
 
-# tup = (2,4,5,3,1,2,7,6,2,8,2)
+tup = (2,4,5,3,1,2,7,6,2,8,2)
 
-# print(tup)
-# print(len(tup))
+print(tup)
+print(len(tup))
 
-# print(tup.count(2))
-# print(tup.count(7))
+print(tup.count(2))
+print(tup.count(7))
 
 
 #### -------del in tuple------------------------
 
-# tup = (2,4,5,3,1,2,7,6,2,8,2)
-# del(tup)
-# print(tup)
+tup = (2,4,5,3,1,2,7,6,2,8,2)
+del(tup)
+print(tup)
 
 
 #### -------joining tuples----------------------
 
-# my_tuple = (1, 2, 3, 'a', 'b', 'c')
-# tup = (2,4,5,3,1,2,7,6,2,8,2)
+my_tuple = (1, 2, 3, 'a', 'b', 'c')
+tup = (2,4,5,3,1,2,7,6,2,8,2)
 
-# newTup = my_tuple + tup
-# print(newTup)
-# print(len(newTup))
+newTup = my_tuple + tup
+print(newTup)
+print(len(newTup))
 
 
 #### -------converting a list into a tuple ----------
 
-# alist = ['red','purple','pink','orange','yellow']
-# print(alist)
+alist = ['red','purple','pink','orange','yellow']
+print(alist)
 
-# tup2 = tuple(alist)
-# print(tup2)
+tup2 = tuple(alist)
+print(tup2)
 
 
 #### --------converting a string into a tuple---------
 
-# string = 'it\'s a brand new day!'
-# print(string)
+string = 'it\'s a brand new day!'
+print(string)
 
-# tup3 = tuple(string)
-# print(tup3)
+tup3 = tuple(string)
+print(tup3)
