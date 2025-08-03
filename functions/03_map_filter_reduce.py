f4c05cc5b9e5cb23a231c3170eb9cb1a85f02dba
Iftekhar <iftars2774@gmail.com>
2025-08-03 15:32:31 +0530

    uncommented the codes

--- functions/03_map_filter_reduce.py
+++ functions/03_map_filter_reduce.py
@@ -7,71 +7,72 @@ It appplies a given function to each item of that given iterable.
 
 '''Returning a cube list using map '''
 
-# li = [1,2,3,4,5]
+li = [1,2,3,4,5]
 
-# def cube(n):
-#     return n**3
+def cube(n):
+    return n**3
 
-# cube_list = list(map(cube,li))
-# print(cube_list)
+cube_list = list(map(cube,li))
+print(cube_list)
 
 
 ## ------------------------------------------------------------------------
 
-# list_of_strings = ['scissors', 'pen', 'bicycle', 'bottle']
-# number_li = []
+list_of_strings = ['scissors', 'pen', 'bicycle', 'bottle']
+number_li = []
 
-# def count_the_len(a):
-#     for i in a:
-#         number_li.append(len(i))
+def count_the_len(a):
+    for i in a:
+        number_li.append(len(i))
 
-# count_the_len(list_of_strings)
-# print(number_li)
+count_the_len(list_of_strings)
+print(number_li)
 
 
-# len_list = list(map(len, list_of_strings))
-# print(len_list)
+len_list = list(map(len, list_of_strings))
+print(len_list)
 
 
 ## ---------------------------------------------------------
 
 '''Converting list of ints into list of string'''
 
-# int_list = [2,4,5,6,3,8]
+int_list = [2,4,5,6,3,8]
 
-# string_list = list(map(str, int_list))
-# print("List Of Strings:",string_list)
+string_list = list(map(str, int_list))
+print("List Of Strings:",string_list)
 
 
 ## ----------------------------------------------------------------------------------------
 
-# numbers = [1, 2, 3, 4, 5]
-# doubled_numbers = list(map(lambda x: x * 2, numbers))
-# print(doubled_numbers)
+numbers = [1, 2, 3, 4, 5]
+doubled_numbers = list(map(lambda x: x * 2, numbers))
+print(doubled_numbers)
 
 
-# ---------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------
 
 '''Adding two lists using map'''
-# alist = [1,2,3,4,5]
-# blist = [6,7,8,9,10]
 
-# added_list = list(map(lambda x,y: x+y, alist, blist))
-# print(added_list)
+alist = [1,2,3,4,5]
+blist = [6,7,8,9,10]
 
-
-# ---------------------------------------------------------------------------------------
-
-# li = [1,2,'Hello', True, 7]
-
-# string_list = list(map(str, li))
-# print(string_list)
+added_list = list(map(lambda x,y: x+y, alist, blist))
+print(added_list)
 
 
-# ---------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------
 
-# num = int(input("Enter a number to print cubes upto it: "))
+li = [1,2,'Hello', True, 7]
 
-# cubes_list = list(map(lambda x: x**3, range(1,num+1)))
-# print(cubes_list)
+string_list = list(map(str, li))
+print(string_list)
+
+
+## ---------------------------------------------------------------------------------------
+
+num = int(input("Enter a number to print cubes upto it: "))
+
+cubes_list = list(map(lambda x: x**3, range(1,num+1)))
+print(cubes_list)
 
