41d758997c9a01479e0bdd9dea879996585c56a0
Iftekhar <iftars2774@gmail.com>
2025-08-06 22:15:31 +0530

    list comprehension completed

---
+++ miscellaneous/01_list_dict_comprehension.py
@@ -0,0 +1,38 @@
+
+#### ---------- LIST COMPREHENSIONS ----------
+
+# print([i for i in range(1,11)])
+
+
+## Create a list of odd values
+
+# oddList = [i for i in range(1,21) if i%2 != 0]
+# print('Odd List:',oddList)
+
+
+# square_list = [i*i for i in range(1,21)]
+# print(square_list)
+
+
+# A list with table of two
+
+# table_of_two = [i*2 for i in range(1,11)]
+# print("Table of 2:",table_of_two)
+
+
+# cities = ["Madrid", "London", "New York", "Berlin", "Tokyo"]
+
+# filtered_cities = [i for i in cities if "o" in i]
+# print(filtered_cities)
+
+
+
+#### -------------- NESTED LISTS --------------
+
+# a =[[i] for i in range(4)]
+# print(a)
+
+# nested_list = [[i for i in range(1,4)] for i in range(4)]
+# print(nested_list)
+
+
