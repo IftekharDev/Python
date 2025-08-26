7ee2335c2a9e7b9cc86e84edb8c0c51a5008ac2c
Iftekhar <iftars2774@gmail.com>
2025-08-26 23:46:39 +0530

    High level functions completed

---
+++ functions/04_functions.py
@@ -0,0 +1,98 @@
+
+####-------- Callback Function----------------
+
+'''
+A callback function in Python is a function that is passed as an argument to another function and
+is executed inside that function.
+'''
+
+# def function_1(func):
+#     return "i am function 1 " + func()
+
+# def function_2():
+#     return "i am function 2"
+
+# result = function_1(function_2)
+# print(result)
+
+
+### -------------------------------------------------------------------------------
+
+# def greet(name, callback):
+#    message = f"Hello, {name}!"
+#    return callback(message)
+
+# def print_message(msg):
+#    print(msg)
+
+# greet("Alice", print_message)
+
+
+#### ------------Nested Function------------------------
+
+'''
+A nested function is a function defined inside another function.
+'''
+
+# def outer():
+#    x = 12
+#    def inner():
+#        print(x)
+   
+#    inner()
+
+# outer()
+
+
+### -------------------------------------------------------------------------------
+
+# def greet(name):
+#    def say_hello():
+#        print(f"Hello, {name}!")
+   
+#    say_hello()
+
+# greet("Alice")
+
+
+
+#### -----------------Closure---------------------
+
+'''
+A closure is a function that retains access to the variables from its enclosing scope, 
+even after the enclosing function has finished executing.
+
+For inner Function to be a closure, it needs to be returned from outerFunction and 
+then called after outerFunction has finished executing.
+'''
+
+# def outer_function(x):
+#    def inner_function(y):
+#        return x + y
+#    return inner_function
+
+# result = outer_function(10)
+# print(result(5))
+
+
+### -------------------------------------------------------------------------------
+
+# def my_func(value):
+#    def multiply(number):
+#        return number * value
+#    return multiply
+
+
+# result = my_func(3)
+
+# print(result(5)) 
+# print(result(10))
+
+
+# def Greet(n):
+#      def Greet_2():
+#            return f"Hello {n}"
+#      return Greet_2
+
+# var = Greet("Ana")
+# print(var())
