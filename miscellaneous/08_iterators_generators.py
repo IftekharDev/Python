bf99e143201f10f0509493855998a17e5019a49c
Iftekhar <iftars2774@gmail.com>
2025-08-26 23:26:09 +0530

    uncommented and formatted the codes

--- miscellaneous/08_iterators_generators.py
+++ miscellaneous/08_iterators_generators.py
@@ -5,12 +5,12 @@
 An Iterable is basically an object that any user can iterate over. ex- a list or a tuple
 '''
 
-# alist = [2,4,6,8,10]
-# print(alist)
-# print(alist[2])
-# print(alist[4])
-# for i in alist:
-#    print(i)
+alist = [2,4,6,8,10]
+print(alist)
+print(alist[2])
+print(alist[4])
+for i in alist:
+   print(i)
 
 
 #### ----------------- Iterators ------------------
@@ -21,9 +21,9 @@ An iterator is generated when we pass an iterable to iter() method. It gives one
 iter( ) converts an iterable into an iterator.
 '''
 
-# alist = [2,4,6,8,10]
-# x = iter(alist)
-# print(x)              # returns an iterator object
+alist = [2,4,6,8,10]
+x = iter(alist)
+print(x)              # returns an iterator object
 
 
 '''
@@ -31,24 +31,24 @@ for accessing values from an iterator we use __next__() {dunder or magic methods
 __next__() method is used for iterating. It returns the next item available for iteration.
 '''
 
-# print(x.__next__())
+print(x.__next__())
 
 ### "next()" is same as "__next__()"
 
-# print(list(x))
-# print(next(x))
-# print(next(x))
-# print(next(x))
-# print(next(x))
+print(list(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
 ## print(next(x))        #this will raise a stop iteration error
 
 
 ### for loop can be used to access it
 
-# for i in x:               # iterator prints a value only once
-#    print(i)              # loop will start from second value
+for i in x:               # iterator prints a value only once
+   print(i)              # loop will start from second value
 
-# print(next(x))              #this will raise a stop iteration error
+print(next(x))              #this will raise a stop iteration error
 
 
 #### -------------------- GENERATOR --------------------
@@ -60,74 +60,75 @@ Generators simplify iterator creation by handling the iterator protocol. Generat
 
  
 
-# def func():
-#    yield "Hello"
+def func():
+   yield "Hello"
 
 
-# string = func()
-# print(string)           #this will return a generator object
+string = func()
+print(string)           #this will return a generator object
 
-# print(string.__next__())
-# or
-# print(next(string))      # this returns the actual value
+print(string.__next__())
+print(next(string))      # this returns the actual value
 
-# for i in string:
-#    print(i)
-#    print(type(i))
-
-
-#-------------------------------------------------------------
-
-# def num():
-#     yield 1               # we can use multiple yield in generator
-#     yield 2
-#     yield 3
-
-# var = num()
-# print(var.__next__())
-# print(var.__next__())
-
-# for i in var:
-#     print(i)
+for i in string:
+   print(i)
+   print(type(i))
 
 
-# -------------------------------------------------------------
+##-------------------------------------------------------------
 
-# def square(n):
-#     sq = n*n
-#     yield sq
+def num():
+    yield 1               # we can use multiple yield in generator
+    yield 2
+    yield 3
 
-# SQUARE = (square(2))
-# print(SQUARE)
-# print(SQUARE.__next__())
+var = num()
+print(var.__next__())
+print(var.__next__())
 
-
-# def even():
-#     i =1
-#     while True:
-#         yield i*i
-#         i+=1
-
-# even_num = even()
-# print(even_num.__next__())
-# print(even_num.__next__())
+for i in var:
+    print(i)
 
 
-# def squares():
+## -------------------------------------------------------------
 
-#    for i in range(2,10):
-#        sq = i*i
-#        yield sq
+def square(n):
+    sq = n*n
+    yield sq
 
-
-# square = squares()
-# print(square)
-# print(square.__next__())
-# print(square.__next__())
+SQUARE = (square(2))
+print(SQUARE)
+print(SQUARE.__next__())
 
 
-# squareList =[]
-# for i in square:
-#    squareList.append(i)
+def even():
+    i =1
+    while True:
+        yield i*i
+        i+=1
 
-# print(squareList)
+even_num = even()
+print(even_num.__next__())
+print(even_num.__next__())
+
+
+## -------------------------------------------------------------
+
+def squares():
+
+   for i in range(2,10):
+       sq = i*i
+       yield sq
+
+
+square = squares()
+print(square)
+print(square.__next__())
+print(square.__next__())
+
+
+squareList =[]
+for i in square:
+   squareList.append(i)
+
+print(squareList)
