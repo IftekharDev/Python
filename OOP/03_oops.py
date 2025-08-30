a32d3dd95add35e4a1662c47333d332ada3bcdfc
Iftekhar <iftars2774@gmail.com>
2025-08-30 13:21:05 +0530

    Some practice questions added

--- OOP/03_oops.py
+++ OOP/03_oops.py
@@ -49,7 +49,7 @@
 # obj_son.eat()
 
 
-# ----------------------------------------------------------------------
+### ----------------------------------------------------------------------
 
 # class Parent:
 #     def display(self):
@@ -71,3 +71,102 @@
 
 # c = Child()
 # c.display()
+
+
+### ----------------------------------------------------------------------
+
+# class Father:
+#     def __init__(self, name, house):
+#         self.name = name
+#         self.house = house
+
+
+# class Son(Father):
+#     pass
+
+
+# objDad = Father("henry","1 house")
+# print(objDad.name)
+
+# obj = Son("jerry", "a house")
+# print(obj.name)
+
+
+### Practice Question: 
+
+'''
+Q. Define a class named Book with three attributes: title, author & pages.
+Implement a method called description() that returns a string representation of the class.
+'''
+
+# class Book:
+#     def __init__(self, title, author, pages):
+#         self.title = title
+#         self.author = author
+#         self.pages = pages
+
+#     def __str__(self):
+#         return f"{self.title} is written by {self.author} and it has {self.pages} pages"
+    
+
+# ice_and_fire = Book("Ice And Fire","George RR Martin", 900 )
+# print(ice_and_fire)
+
+
+# -----------------------------------------------------------
+
+# class Parent:
+#    def display(self):
+#        print("Hello1")
+
+#    def display(self,a):
+#        print("Hi!!",a)
+
+
+# class Child(Parent):
+#    def display(self,a = None):
+#        print("Hello2")
+#        super().display(a)   
+
+
+# c = Child()
+# c.display()
+# c.display("there")
+
+
+### ------------------------------------------------------------
+
+'''
+Q. Define a class named Student with three attributes: name, student_id, and grade.
+Implement a method called get_info() that returns a string representation of the class.
+Then, define a subclass named Undergraduate that inherits from Student 
+and includes two additional attributes: major and year.
+Implement a method called get_details() that returns a string 
+representation of the Undergraduate class, including all attributes.
+'''
+
+
+# class Student:
+#     def __init__(self, name, student_id, grade):
+#         self.name = name
+#         self.student_id = student_id
+#         self.grade = grade
+
+#     def get_info(self):
+#         return f"Hiii I'm {self.name}, my student id is {self.student_id} and My grade is {self.grade}"
+    
+
+# class Undergraduate(Student):
+#     def __init__(self,name, student_id, grade, major, year):
+#         super().__init__(name, student_id, grade)
+#         self.major = major
+#         self.year = year
+
+#     def get_details(self):
+#         return f"Hiii I'm {self.name}, my student id is {self.student_id} and I'm in grade {self.grade}. I'm doing major in {self.major} and I'm in year {self.year}"
+    
+
+# obj = Undergraduate("Zain", 101, "A", "CSE", 2025)
+# print(obj.get_details())
+# print(obj.get_info())
+
