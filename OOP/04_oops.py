14fe37d06819f90c8a63cf24f27cd361cfb84b7f
Iftekhar <iftars2774@gmail.com>
2025-08-30 14:19:46 +0530

    uncommented and formatted the codes

--- OOP/04_oops.py
+++ OOP/04_oops.py
@@ -7,23 +7,24 @@ rather than its explicit type. If it walks like a duck and quacks like a duck, i
 '''
 
 
-# class Bird:
-#     def fly(self):
-#         return "a bird flies in the sky"
+class Bird:
+    def fly(self):
+        return "a bird flies in the sky"
 
 
-# class AirPlane:
-#     def fly(self):
-#         return "AirPlane flies in the sky"
+class AirPlane:
+    def fly(self):
+        return "AirPlane flies in the sky"
 
 
-# def make_it_fly(flyable_object):
-#     return flyable_object.fly()
+def make_it_fly(flyable_object):
+    return flyable_object.fly()
 
-# sparrow = Bird()
-# jet = AirPlane()
-# make_it_fly(sparrow)
-# make_it_fly(jet) 
+
+sparrow = Bird()
+jet = AirPlane()
+make_it_fly(sparrow)
+make_it_fly(jet) 
 
 
 '''
@@ -33,40 +34,42 @@ works with any object that has a fly() method, regardless of the object's actual
 
 ###---------------------------------------------------------
 
-# class Duck:
-#     def quack(self):
-#         print("Quack, quack!")
+class Duck:
+    def quack(self):
+        print("Quack, quack!")
 
-#     def fly(self):
-#         print("Flap, flap!")
-
-
-# class Person:
-#     def quack(self):
-#         print("I'm quacking like a duck!")
-
-#     def fly(self):
-#         print("I'm waving my arms and pretending to fly!")
+    def fly(self):
+        print("Flap, flap!")
 
 
-# This function doesn't care about the type of the object.
-# It only cares that the object has 'quack' and 'fly' methods.
-# def make_it_quack_and_fly(thing):
-#     try:
-#         thing.quack()
-#         thing.fly()
-#     except AttributeError as e:
-#         print(f"This object can't do that: {e}")
+class Person:
+    def quack(self):
+        print("I'm quacking like a duck!")
 
-# Create instances of our classes
-# donald = Duck()
-# john = Person()
+    def fly(self):
+        print("I'm waving my arms and pretending to fly!")
 
-# print("Calling the function with a Duck:")
-# make_it_quack_and_fly(donald)
 
-# print("\nCalling the function with a Person:")
-# make_it_quack_and_fly(john)
+'''
+This function doesn't care about the type of the object. It only cares that the object has 'quack' and 'fly' methods.
+'''
+def make_it_quack_and_fly(thing):
+    try:
+        thing.quack()
+        thing.fly()
+    except AttributeError as e:
+        print(f"This object can't do that: {e}")
+
+###Create instances of our classes
+
+donald = Duck()
+john = Person()
+
+print("Calling the function with a Duck:")
+make_it_quack_and_fly(donald)
+
+print("\nCalling the function with a Person:")
+make_it_quack_and_fly(john)
 
 
 ####---------------- Abstract Class -------------------------
@@ -76,136 +79,158 @@ Abstract Class - An abstract class is a class that cannot be instantiated direct
 Abstract methods, on the other hand, are methods declared within an abstract class but without any implementation.
 '''
 
-# class Vehicle:
-#     def drive(self):
-#         pass
+class Vehicle:
+    def drive(self):
+        pass
 
-# obj = Vehicle()
-# print(obj)
+obj = Vehicle()
+print(obj)
 
 ### this is working normally as Vahicle isn't an abstract class yet
 
 
 ###------------------------------------------------------------------
 
-# from abc import ABC, abstractmethod           ##abstract base classes
+from abc import ABC, abstractmethod           ##abstract base classes
 
-# class Vehicle(ABC):
-#     @abstractmethod
-#     def drive(self):
-#         pass
+class Vehicle(ABC):
+    @abstractmethod
+    def drive(self):
+        pass
 
 ###obj = Vehicle()             ## can't be initiated
 ###print(obj)
 
 
-# class Car(Vehicle):
-#     def drive(self):
-#         return "Car is being driven on the road."
+class Car(Vehicle):
+    def drive(self):
+        return "Car is being driven on the road."
 
-# class Airplane(Vehicle):  
-#     def drive(self):
-#         return "The Airplane is flying in the sky."
+class Airplane(Vehicle):  
+    def drive(self):
+        return "The Airplane is flying in the sky."
     
-# objCar = Car()
-# print(objCar.drive())
-# objAirplane = Airplane()
-# print(objAirplane.drive())
+objCar = Car()
+print(objCar.drive())
+objAirplane = Airplane()
+print(objAirplane.drive())
 
 
 ###------------------------------------------------------------------
 
-# No abstract Vehicle class here
+### No abstract Vehicle class here
 
-# class Car:
-#     def drive(self):
-#         print("Car is being driven on the road.")
+class Car:
+    def drive(self):
+        print("Car is being driven on the road.")
 
-# class Airplane:
-#     def fly(self):  # Uh oh, a different method name!
-#         print("The Airplane is flying in the sky.")
+class Airplane:
+    def fly(self):  ## Uh oh, a different method name!
+        print("The Airplane is flying in the sky.")
 
-# class Bicycle:
-#     # This developer forgot to add any movement method!
-#     def pedal(self):
-#         print("Pedaling the bicycle.")
+class Bicycle:
+    ## This developer forgot to add any movement method!
+    def pedal(self):
+        print("Pedaling the bicycle.")
 
-# def start_trip(vehicles):
-#     print("--- Starting trip ---")
-#     for vehicle in vehicles:
-#         # This function EXPECTS every vehicle to have a .drive() method
-#         vehicle.drive()
-#     print("--- Trip finished ---")
+def start_trip(vehicles):
+    print("--- Starting trip ---")
+    for vehicle in vehicles:
+        ## This function EXPECTS every vehicle to have a .drive() method
+        vehicle.drive()
+    print("--- Trip finished ---")
 
 
-# Let's create our fleet
-# my_car = Car()
-# my_airplane = Airplane()
-# my_bike = Bicycle()
+### Let's create our fleet
 
-# # Let's try to start a trip with a car. This works fine.
-# start_trip([my_car])
+my_car = Car()
+my_airplane = Airplane()
+my_bike = Bicycle()
 
-# # Now, what happens when we mix them?
-# # This will CRASH!
-# try:
-#     all_vehicles = [my_car, my_airplane, my_bike]
-#     start_trip(all_vehicles)
-# except AttributeError as e:
-#     print(f"\nERROR: The program crashed because of: {e}")
+### Let's try to start a trip with a car. This works fine.
+
+start_trip([my_car])
+
+### Now, what happens when we mix them?
+### This will CRASH!
+
+try:
+    all_vehicles = [my_car, my_airplane, my_bike]
+    start_trip(all_vehicles)
+except AttributeError as e:
+    print(f"\nERROR: The program crashed because of: {e}")
 
 
 ###------------------------------------------------------------------
 
-# from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 
-# # The Contract: Anything that is a Vehicle MUST have a 'drive' method.
-# class Vehicle(ABC):
-#     @abstractmethod
-#     def drive(self):
-#         pass
+''' The Contract: Anything that is a Vehicle MUST have a 'drive' method. '''
 
-# # This class follows the contract. It's a valid Vehicle.
-# class Car(Vehicle):
-#     def drive(self):
-#         print("Car is being driven on the road.")
-
-# # This class ALSO follows the contract. It's a valid Vehicle.
-# # We force the developer to name the method 'drive'.
-# class Airplane(Vehicle):
-#     def drive(self):
-#         print("The Airplane is flying in the sky.")
-
-# # Now, let's see what happens when a developer makes a mistake
-# class Bicycle(Vehicle):
-#     # The developer forgot to implement the 'drive' method!
-#     def pedal(self):
-#         print("Pedaling the bicycle.")
-
-# # The function is the same, but now it's SAFE.
-# # It can TRUST that any object of type Vehicle will have a .drive() method.
-# def start_trip(vehicles):
-#     print("--- Starting trip ---")
-#     for vehicle in vehicles:
-#         vehicle.drive()
-#     print("--- Trip finished ---")
+class Vehicle(ABC):
+    @abstractmethod
+    def drive(self):
+        pass
 
 
-# print("Creating a Car:")
-# my_car = Car()
-# print("Creating an Airplane:")
-# my_airplane = Airplane()
+''' This class follows the contract. It's a valid Vehicle. '''
 
-# print("\nTrying to create a Bicycle...")
-# try:
-#     # This line will now FAIL IMMEDIATELY!
-#     my_bike = Bicycle()
-# except TypeError as e:
-#     print(f"ERROR: Could not create Bicycle. {e}")
+class Car(Vehicle):
+    def drive(self):
+        print("Car is being driven on the road.")
 
-# # Since you can't even create a non-compliant object,
-# # the start_trip function can never crash in this way.
-# print("\nRunning a trip with the valid vehicles:")
-# valid_vehicles = [my_car, my_airplane]
-# start_trip(valid_vehicles)
 
+''' 
+This class ALSO follows the contract. It's a valid Vehicle.
+We force the developer to name the method 'drive'.
+'''
+class Airplane(Vehicle):
+    def drive(self):
+        print("The Airplane is flying in the sky.")
+
+
+''' Now, let's see what happens when a developer makes a mistake '''
+class Bicycle(Vehicle):
+    ## The developer forgot to implement the 'drive' method!
+    def pedal(self):
+        print("Pedaling the bicycle.")
+
+''' 
+The function is the same, but now it's SAFE.
+It can TRUST that any object of type Vehicle will have a .drive() method.
+'''
+def start_trip(vehicles):
+    print("--- Starting trip ---")
+    for vehicle in vehicles:
+        vehicle.drive()
+    print("--- Trip finished ---")
+
+
+print("Creating a Car:")
+my_car = Car()
+print("Creating an Airplane:")
+my_airplane = Airplane()
+
+
+print("\nTrying to create a Bicycle...")
+try:
+    ## This line will now FAIL IMMEDIATELY!
+    my_bike = Bicycle()
+except TypeError as e:
+    print(f"ERROR: Could not create Bicycle. {e}")
+
+
+'''
+Since you can't even create a non-compliant object,
+the start_trip function can never crash in this way.
+'''
+print("\nRunning a trip with the valid vehicles:")
+valid_vehicles = [my_car, my_airplane]
+start_trip(valid_vehicles)
+
+
+'''
+abstraction isn't for changing the output; it's for building a strong, error-resistant 
+foundation for your program. It's a tool for managing complexity and ensuring that all the different 
+parts of your system work together correctly.
+'''
